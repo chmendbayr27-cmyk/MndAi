2d354f12dff911c3ce694fad88a7985f3cfebb71
chmendbayr27-cmyk <chmendbayr27@gmail.com>
2026-07-22 23:37:30 -0300

    Update app.js

--- mndai-complete/backend/src/app.js
+++ mndai-complete/backend/src/app.js
@@ -23,7 +23,7 @@ const { errorHandler } = require('./middleware/errorHandler');
 const { validateInput } = require('./middleware/validation');
 
 const app = express();
-
+app.set('trust proxy', 1);
 // Security Middleware
 app.use(helmet());
 app.use(cors({
