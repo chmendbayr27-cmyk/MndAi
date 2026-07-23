a188ce4c90e3e9d36cf12eb2f79667d20db2a0d1
chmendbayr27-cmyk <chmendbayr27@gmail.com>
2026-07-22 22:13:32 -0300

    Create Bookings.js

---
+++ mndai-complete/frontend/src/pages/Bookings.js
@@ -0,0 +1,7 @@
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import Chat from './pages/Chat';
+import Bookings from './pages/Bookings';
+import Customers from './pages/Customers';
+import Settings from './pages/Settings';
