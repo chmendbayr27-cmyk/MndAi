27dfa96f271daa8ac3901a23f9626a18c3b7087c
chmendbayr27-cmyk <chmendbayr27@gmail.com>
2026-07-22 23:21:55 -0300

    Update Register.js

--- mndai-complete/frontend/src/pages/Register.js
+++ mndai-complete/frontend/src/pages/Register.js
@@ -1,12 +1,1 @@
-import React from "react";
 
-function Register() {
-  return (
-    <div style={{ padding: "40px" }}>
-      <h1>Register</h1>
-      <p>Registration page coming soon.</p>
-    </div>
-  );
-}
-
-export default Register;
